f60b0fa30d04fe4fbf9667f077a88df674f35950
Felipe Farias <felipecostasdc@gmail.com>
2022-03-24 20:30:56 -0300

    implement happy paths for filtering movies

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,18 +1,27 @@
 Given /^the following movies exist:/ do |movies_table|
   movieHashes = movies_table.hashes   # {"title"=>"Aladdin", "rating"=>"G", "release_date"=>"25-Nov-1992"}, {"title"=> "...
-
+  
   movieHashes.each do |movie|
     Movie.create! movie
   end
-
+  
 end
 
-Given /I check the following ratings: (.*)$/ do |ratings|
-  ratings.split(',').each do |rating|
-    page.check("ratings_#{rating.strip}")
+Given /I (un)?check the following ratings: (.*)$/ do |uncheck, ratings|
+  ratings.split(', ').each do |rating|
+    if !uncheck
+      page.check("ratings_#{rating}")
+    else
+      page.uncheck("ratings_#{rating}")
+    end
   end
 end
 
 Given /I check all ratings/ do
   steps %Q{Given I check the following ratings: #{Movie.all_ratings.join(', ')}}
+end
+
+Then /I should see all of the movies/ do
+  # page.all("#movies tbody tr").size.should == Movie.all.size
+  page.assert_selector('#movies tbody tr', count: Movie.all.size)
 end
